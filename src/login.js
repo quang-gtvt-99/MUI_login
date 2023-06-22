da123ddd3f43bc4b1f8cbdec58e2e2a94fba69d4
quang-gtvt-99 <74550494+quang-gtvt-99@users.noreply.github.com>
2023-06-23 02:05:48 +0700

    [Add] login + register

---
+++ src/login.js
@@ -0,0 +1,129 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+  Box,
+  Button,
+  Stack,
+  TextField,
+  Typography
+} from '@mui/material';
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Sign from './register';
+const Page = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      submit: null
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Password is required')
+    }),
+  });
+  return (
+    <Router>
+      <Box
+        sx={{
+          backgroundColor: 'background.paper',
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: '100px',
+            width: '100%'
+          }}
+        >
+          <div>
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
+            >
+              <Typography variant="h4">
+                Login
+              </Typography>
+              <Typography
+                color="text.secondary"
+                variant="body2"
+              >
+                Don&apos;t have an account?
+                &nbsp;
+                  <Link
+                    to="/Register"
+                  >
+                    Register
+                  </Link>
+                  <Routes>
+                    <Route path="/Register" component={Sign} />           
+                  </Routes>
+              </Typography>
+            </Stack>
+              <form
+                noValidate
+                onSubmit={formik.handleSubmit}
+              >
+                <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Email Address"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={!!(formik.touched.password && formik.errors.password)}
+                    fullWidth
+                    helperText={formik.touched.password && formik.errors.password}
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+                {formik.errors.submit && (
+                  <Typography
+                    color="error"
+                    sx={{ mt: 3 }}
+                    variant="body2"
+                  >
+                    {formik.errors.submit}
+                  </Typography>
+                )}
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  type="submit"
+                  variant="contained"
+                >
+                  Continue
+                </Button>
+              </form>
+           
+          </div>
+        </Box>
+      </Box>
+      </Router>
+  );
+};
+
+export default Page;
